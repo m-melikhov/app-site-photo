2096c3dc2aa7bfba64b6f4ae2bf0cc1022d9ac2b
Maxim Melikhov <melekhov25@gmail.com>
2024-05-19 22:50:36 +0300

    form commit

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -1,33 +1,46 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
 import axios from 'axios';
-import '../Styles/ConcactForm.css';
+import '../Styles/ContactForm.css'; // Виправлений шлях до стилів
 
-function ContactForm() {
+export default function ContactForm() { // Виправлена назва компонента
+  const TOKEN = "7111363762:AAEf795-szr9fGDGwjVJvBLcrTjQ2zQKaE8";
+  const CHAT_ID = "7102789163";
+  const URI_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
   const [instagram, setInstagram] = useState('');
   const [comment, setComment] = useState('');
-  const [message, setMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState("");
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    try {
-      const response = await axios.post('/send-message', {
-        name,
-        phone,
-        instagram,
-        comment,
+    let message = `<b>Заявка с сайта!</b>\n`;
+    message += `<b>Імʼя: </b> ${name}\n`;
+    message += `<b>Номер телефону: </b> ${phone}\n`;
+    message += `<b>Інстаграм: </b> ${instagram}\n`;
+    message += `<b>Повідомлення: </b> ${comment}`;
+
+    axios
+      .post(URI_API, {
+        chat_id: CHAT_ID,
+        parse_mode: "html",
+        text: message,
+      })
+      // eslint-disable-next-line no-unused-vars
+      .then((res) => {
+        setName('');
+        setPhone('');
+        setInstagram('');
+        setComment('');
+        setSuccessMessage("Повідомлення надіслано");
+      })
+      .catch((err) => {
+        console.warn(err);
+        setSuccessMessage("Помилка відправки повідомлення.");
       });
-      if (response.data.success) {
-        setMessage('Повідомлення успішно надіслано!');
-      } else {
-        setMessage('Щось пішло не так. Спробуйте ще раз.');
-      }
-    } catch (error) {
-      setMessage('Помилка відправки повідомлення.');
-    }
   };
 
   return (
@@ -36,25 +49,23 @@ function ContactForm() {
       <form onSubmit={handleSubmit}>
         <label>
           Імʼя:
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} required />
+          <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} required />
         </label>
         <label>
           Телефон:
-          <input type="tel" value={phone} onChange={(e) => setPhone(e.target.value)} required />
+          <input type="tel" name="tel" value={phone} onChange={(e) => setPhone(e.target.value)} required />
         </label>
         <label>
           Інстаграм:
-          <input type="text" value={instagram} onChange={(e) => setInstagram(e.target.value)} required />
+          <input type="text" name="inst" value={instagram} onChange={(e) => setInstagram(e.target.value)} required />
         </label>
         <label>
           Коментар:
-          <textarea value={comment} onChange={(e) => setComment(e.target.value)} required />
+          <textarea name="textarea" value={comment} onChange={(e) => setComment(e.target.value)} required />
         </label>
         <button type="submit">Надіслати</button>
       </form>
-      {message && <p>{message}</p>}
+      {successMessage && <p>{successMessage}</p>}
     </section>
   );
 }
-
-export default ContactForm;
